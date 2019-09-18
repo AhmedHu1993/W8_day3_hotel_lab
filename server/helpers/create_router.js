d0325f718572197914fe5d3de04cda119347fd83
Ahmed Hussein <ahmd.adl.elsayed@gmail.com>
2019-09-18 11:52:47 +0100

    worked on client routes

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -5,6 +5,47 @@ const createRouter = function(collection) {
     
     const router = express.Router();
 
+    // Show
+    router.get('/', (req, res) => {
+        collection.find().toArray()
+            .then((docs) => res.json(docs))
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                    status: 500,
+                    message: err
+                })
+            });
+    }),
+
+    // Create
+    router.post('/', (req, res) => {
+        const newData = req.body;
+        collection.insertOne(newData)
+            .then((doc) => res.json(doc.ops[0]))
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                    status: 500,
+                    message: err
+                })
+            })
+    });
+
+    // Destroy
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id;
+        collection.deleteOne({ _id: ObjectId(id)})
+            .then(() => collection.find().toArray())
+            .then((docs) => res.json(docs))
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                    status: 500,
+                    message: err
+                })
+            })
+    });
 
     return router;
 };
